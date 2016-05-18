eed839909c414d21ff25827da8340be3c0c3a83f
Dennis van Bennekom <vanbennekomdennis@gmail.com>
2016-05-18 14:04:30 +0200

    Add date

--- data.php
+++ data.php
@@ -1,6 +1,6 @@
 <?php
 $value = $_POST['value'];
-
+$date = date('D M d Y H:i:s 0');
 $file = 'sound.txt';
 $content = file_get_contents($file);
-$file_put_contents($file, $value . '.' . $content);
+$file_put_contents($file, '\n' . $date . '-' . $content);
