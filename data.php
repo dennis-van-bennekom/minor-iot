59a1c3c14efce4c44708743c495990464ea3588d
Dennis van Bennekom <vanbennekomdennis@gmail.com>
2016-05-18 14:07:32 +0200

    Fix

--- data.php
+++ data.php
@@ -3,4 +3,4 @@ $value = $_POST['value'];
 $date = date('D M d Y H:i:s 0');
 $file = 'sound.txt';
 $content = file_get_contents($file);
-$file_put_contents($file, '\n' . $date . '-' . $content);
+$file_put_contents($file, $date . '-' . $content);
